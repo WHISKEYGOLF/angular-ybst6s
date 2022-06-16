c6a313f61d3731dccb98683b6152de213d908c81
WHISKEYGOLF <softwareengineer.pr011@gmail.com>
2022-06-16 14:10:57 -0700

    cart service: httpClientModule, rxjs; shipping-component.  7 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,31 +1,61 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, Subject, Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { Product } from './products';
 
+export interface Price {
+  type: string;
+  price: number;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
   items: Product[] = [];
   itemsChanged = new Subject<Product[]>();
+  price = new BehaviorSubject<Price | null>(null);
+  $currentPrice = this.price.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  addToCart(product: Product){
+  addToCart(product: Product) {
     this.items = [...this.items, product];
     this.itemsChanged.next(this.items);
   }
 
   getItems() {
-    this.itemsChanged.subscribe(
-      (items) => {
-        this.items = items;
-      });
-      return [...this.items];
+    this.itemsChanged.subscribe((items) => {
+      this.items = items;
+    });
+    return [...this.items];
   }
 
   clearCart() {
+    alert('clearing cart ... ');
     this.items = [];
     this.itemsChanged.next([]);
+  }
+
+  getShippingPrices() {
+    return this.http
+      .get<{ type: string; price: number }[]>('/assets/shipping.json')
+      .pipe(
+        catchError(this.handleError),
+        tap((priceData) => {
+          alert(JSON.stringify(priceData));
+        })
+      );
+  }
+
+  private handleError(errorRes: HttpErrorResponse): Observable<never> {
+    let errorMessage = errorRes.error.error.message;
+
+    if (!errorRes.error || !errorRes.error.error) {
+      errorMessage = 'An unknow error occured.';
+      return throwError(errorMessage);
+    }
+    return throwError(errorMessage);
   }
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
   items = this.cartService.getItems();
-  
-  constructor(private cartService: CartService) { }
 
-  ngOnInit(): void {
+  constructor(private cartService: CartService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  emptyCart() {
+    this.cartService.clearCart();
+    this.router.navigate(['/']);
   }
 
+  purhcase() {
+    // ..
+  }
 }
